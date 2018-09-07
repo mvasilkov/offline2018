298bd61a71df8c6f17fbd8639247469ba28de1b3
Mark Vasilkov <mvasilkov@gmail.com>
2018-09-07 20:20:13 +0300

    Levels: intro sequence

--- javascript/levels.js
+++ javascript/levels.js
@@ -1,10 +1,40 @@
 "use strict";
 const HOLE = -255;
+/* Copy paste:
+    {
+        title: '',
+        cols: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        lasers: [],
+    },
+*/
 const LEVELS = [
     {
         title: 'You Are Now Entering',
         cols: [60, 60, 60, 60, 60, 60, 60, 60, 60, 40, 40, 20, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         lasers: [],
+        ez: true,
+    },
+    {
+        title: 'Jump Around',
+        cols: [0, 0, 0, 0, 0, 0, 0, 0, HOLE, HOLE, HOLE, HOLE, HOLE, HOLE, 0, 0, 0, 0, 0, 0, 0, 0],
+        lasers: [],
+        ez: true,
+    },
+    {
+        title: 'Double Jump',
+        cols: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 310, 310, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        lasers: [],
+        ez: true,
+    },
+    {
+        title: 'Beware Laser Beam',
+        cols: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 140, 140, 0, 0, 0, 0, 0, 0, 0, 0],
+        lasers: [
+            {
+                act: 1.05,
+                pos: 60,
+            },
+        ],
         ez: true,
     },
     {
--- typescript/levels.ts
+++ typescript/levels.ts
@@ -13,11 +13,42 @@ interface ILevel {
 
 const HOLE = -255
 
+/* Copy paste:
+    {
+        title: '',
+        cols: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        lasers: [],
+    },
+*/
+
 const LEVELS: ILevel[] = [
     {
         title: 'You Are Now Entering',
         cols: [60, 60, 60, 60, 60, 60, 60, 60, 60, 40, 40, 20, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         lasers: [],
+        ez: true,
+    },
+    {
+        title: 'Jump Around',
+        cols: [0, 0, 0, 0, 0, 0, 0, 0, HOLE, HOLE, HOLE, HOLE, HOLE, HOLE, 0, 0, 0, 0, 0, 0, 0, 0],
+        lasers: [],
+        ez: true,
+    },
+    {
+        title: 'Double Jump',
+        cols: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 310, 310, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        lasers: [],
+        ez: true,
+    },
+    {
+        title: 'Beware Laser Beam',
+        cols: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 140, 140, 0, 0, 0, 0, 0, 0, 0, 0],
+        lasers: [
+            {
+                act: 1.05,
+                pos: 60,
+            },
+        ],
         ez: true,
     },
     {
